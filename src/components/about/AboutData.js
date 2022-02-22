56cbbc44a0fbe7dc22bdc7ca559b046050cbf51f
Michael Purcella <mjpurcella@gmail.com>
2022-02-21 22:27:46 -0800

    updated about

--- src/components/about/AboutData.js
+++ src/components/about/AboutData.js
@@ -1,5 +1,5 @@
 let aboutData = {
-	value: '"For thousands of years people all over the world have looked to the palms of their hands to uncover the truths of their feelings and character. Clues about love, fortune, relationships, and health - all revealed through the mysterious nature of the lines on our palms. They serve as a map for the future - a story told through intricate patterns. Though these lines act as a compass to guide us, none have shown to be more transformative than the Heart Line."',
+	value: '"For thousands of years people all over the world have looked to the palms of our hands to uncover the truths of our feelings and character. Clues about love, fortune, relationships, and health - all revealed through the mysterious nature of the lines on our palms. They serve as a map for the future - a story told through intricate patterns. Though these lines act as a compass to guide us, none have shown to be more transformative than the Heart Line."',
 	className: 'about-text'
 };
 
